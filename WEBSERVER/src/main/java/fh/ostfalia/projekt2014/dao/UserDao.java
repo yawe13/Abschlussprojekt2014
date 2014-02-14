0d25c2500a915c10ae4f3027d473d65a71236fb6
Yannick <Yannick@YannickRechner>
2014-02-14 11:02:37 +0100

    Webserver mit Musikdienst(Gesamtprojekt) gemerget

--- WEBSERVER/src/main/java/fh/ostfalia/projekt2014/dao/UserDao.java
+++ WEBSERVER/src/main/java/fh/ostfalia/projekt2014/dao/UserDao.java
@@ -6,9 +6,18 @@ package fh.ostfalia.projekt2014.dao;
 
 import fh.ostfalia.projekt2014.model.User;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 
 /**
  *
@@ -16,12 +25,32 @@ import javax.persistence.PersistenceContext;
  */
 @Stateless
 public class UserDao implements UserDaoLocal {
-    @PersistenceContext
+     @PersistenceContext
     private EntityManager em;
+    @Resource
+    UserTransaction ut;
     
     @Override
     public void addUser(User user) {
-        em.persist(user);
+         try {
+             ut.begin();
+             em.persist(user);
+             ut.commit();
+         } catch (NotSupportedException ex) {
+             Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (SystemException ex) {
+             Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (RollbackException ex) {
+             Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (HeuristicMixedException ex) {
+             Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (HeuristicRollbackException ex) {
+             Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (SecurityException ex) {
+             Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (IllegalStateException ex) {
+             Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+         }
     }
 
     @Override
